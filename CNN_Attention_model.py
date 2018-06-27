ab65264284059bfc4ef094482ef2af1413d984b4
weinan1997 <nathaniel_wei@outlook.com>
2018-06-27 11:07:53 +0800

    Fix CNN_Attention.

--- CNN_Attention_model.py
+++ CNN_Attention_model.py
@@ -16,26 +16,25 @@ class CNN_Attention_Sentence(nn.Module):
         self.sentence_l = args["remain_l"]
         
         self.convs = nn.ModuleList([nn.Conv2d(1, self.kernel_num, (k, self.vec_len)) for k in self.kernel_sizes])
-        self.attentions = nn.ModuleList([nn.Linear(self.sentence_l-k+1, self.sentence_l-k+1) for k in self.kernel_sizes])
-        self.att_weights = nn.ParameterList([nn.Parameter(torch.rand(self.sentence_l-k+1, 1)) for k in self.kernel_sizes])
+        self.attention = nn.Linear(self.sentence_l, self.sentence_l)
+        self.att_weight = nn.Parameter(torch.rand(self.sentence_l, 1))
         self.dropout = nn.Dropout(self.dropout)
-        self.fc = nn.Linear((self.sentence_l+1)*len(self.kernel_sizes)-sum(self.kernel_sizes), 2)
+        self.fc = nn.Linear(self.sentence_l, 2)
 
     def forward(self, x):
         x = x.unsqueeze(1)
         Xs = []
         for i in range(self.filter_num):
-            h = F.relu(self.convs[i](x))
+            h = F.pad(x, (0, 0, 0, self.kernel_sizes[i]-1))
+            h = F.relu(self.convs[i](h))
             h = h.squeeze(3)
-            u = self.attentions[i](h)
-            u = F.tanh(u)
-            u = torch.matmul(u, self.att_weights[i])
-            u = F.softmax(u, dim=1)
-            u = u.permute(0, 2 ,1)
-            output = torch.bmm(u, h)
-            output = output.squeeze(1)
-            Xs.append(output)
+            Xs.append(h)
         x = torch.cat(Xs, 1)
+        u = F.tanh(self.attention(x))
+        u = F.softmax(torch.matmul(u, self.att_weight), dim=1)
+        u = u.permute(0, 2, 1)
+        x = torch.bmm(u, x)
+        x = x.squeeze(1)
         x = self.dropout(x)
         x = self.fc(x)
         return x
