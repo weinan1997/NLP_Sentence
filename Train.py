5444fc2a383efadcff5e8bdbc57a362de0308174
weinan1997 <nathaniel_wei@outlook.com>
2018-07-29 14:48:38 +0800

    Minor fix.

--- Train.py
+++ Train.py
@@ -91,26 +91,54 @@ def train(train_set, dev_set, model, args):
 
 def eval(data_set, model, args):
     model.eval()
-    feature, target = data_set[0:2]
-    feature = torch.tensor(np.array(feature))
-    target = torch.tensor(np.array(target))
+    feature_all, target_all = data_set[0:2]
+    domain_all = []
+    if args["data_set"] == "all":
+        domain_all = data_set[2]
+    feature_all = torch.tensor(np.array(feature_all))
+    target_all = torch.tensor(np.array(target_all))
+    domain_all = torch.tensor(np.array(domain_all))
+
+    batch_size = args["batch_size"]
+    input_num = len(feature_all)
+    batch_num = input_num//batch_size
+    if input_num % batch_size != 0:
+        batch_num = batch_num + 1
     
     if torch.cuda.is_available():
-        feature = feature.cuda(args["GPU"])
-        target = target.cuda(args["GPU"])
+        feature_all = feature_all.cuda(args["GPU"])
+        target_all = target_all.cuda(args["GPU"])
+        domain_all = domain_all.cuda(args["GPU"])
 
-    if args["model"] == "dam":
-        output, _ = model(feature)
-    elif args["model"] == "gs":
-        output, _, _ = model(feature)
-    else:
-        output = model(feature)
-    loss = F.cross_entropy(output, target)
-    correct = (torch.max(output, 1)[1].view(target.size()) == target).sum()
-    size = len(feature)
-    accuracy = 100.0 * float(correct)/size
-    print('Evaluation - loss: {:.6f}  acc: {:.4f}%({}/{}) \n'.format(loss.item(), 
+    ave_loss = 0.0
+    total_correct = 0
+
+    for batch in range(0, batch_num):
+        if batch == batch_num-1:
+            feature = feature_all[batch*batch_size:]
+            target = target_all[batch*batch_size:]
+            domain = domain_all[batch*batch_size:]
+        else:
+            feature = feature_all[batch*batch_size:(batch+1)*batch_size]
+            target = target_all[batch*batch_size:(batch+1)*batch_size]
+            domain = domain_all[batch*batch_size:(batch+1)*batch_size]
+        if args["model"] == "dam":
+            output, d = model(feature)
+            loss = F.cross_entropy(output, target, size_average=False) + args["regular"]*F.cross_entropy(d, domain, size_average=False)
+        elif args["model"] == "gs":
+            output, general_output, specific_outputs = model(feature)
+            loss = model.loss(output, general_output, specific_outputs, target, domain, args["lambda1"], args["lambda2"])*len(feature)
+        else:
+            output = model(feature)
+            loss = F.cross_entropy(output, target, size_average=False)
+        correct = (torch.max(output, 1)[1].view(target.size()) == target).sum()
+        ave_loss = ave_loss + loss
+        total_correct = total_correct + correct
+    size = len(feature_all)
+    ave_loss = ave_loss / size
+    accuracy = 100.0 * float(total_correct)/size
+    print('Evaluation - loss: {:.6f}  acc: {:.4f}%({}/{}) \n'.format(ave_loss.item(), 
                                                                        accuracy, 
-                                                                       correct, 
+                                                                       total_correct, 
                                                                        size))
     return accuracy    
