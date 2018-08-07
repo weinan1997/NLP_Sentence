18e23274121c65c02dc7f66f6a7fba8daf12513c
weinan1997 <nathaniel_wei@outlook.com>
2018-08-07 16:03:39 +0800

    Change CNN dropout inplace to True.

--- CNN_model.py
+++ CNN_model.py
@@ -28,7 +28,7 @@ class CNN_Sentence(nn.Module):
         self.embedding = nn.Embedding(self.word2vec.shape[0], self.vec_len)
         self.embedding.weight.data.copy_(torch.from_numpy(self.word2vec))
         self.convs = nn.ModuleList([nn.Conv2d(1, self.kernel_num, (k, self.vec_len)) for k in self.kernel_sizes])
-        self.dropout = nn.Dropout(self.dp)
+        self.dropout = nn.Dropout(self.dp, inplace=True)
         self.fc = nn.Linear(self.filter_num*self.kernel_num, 2)
         
     def forward(self, x):
@@ -42,7 +42,7 @@ class CNN_Sentence(nn.Module):
             Xs.append(temp)
         x = torch.cat(Xs, 1)
         x = x.squeeze(2)
-        x = self.dropout(x)
+        self.dropout(x)
         x = self.fc(x)
         return x
     
